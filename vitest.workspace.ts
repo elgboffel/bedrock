528a0d8789044576a8cdf3b5fc12806bfb6e83a7
Johnny Kristensen <johnny2900@outlook.dk>
2026-05-19 16:26:28 +0200

    chore(repo): add telemetry package to vitest workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,6 +3,7 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   "packages/server",
   "packages/database",
+  "packages/telemetry",
   "apps/api",
   "apps/web",
 ]);
